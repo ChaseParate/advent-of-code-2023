a23dc114e5d0e027f2a05e981478d258c193f9f3
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2023-12-01 00:14:43 -0700

    Refactor code and add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let puzzle_input = include_str!("../puzzle_input.txt");
 
@@ -9,20 +11,27 @@ fn part_one(puzzle_input: &str) -> u32 {
     puzzle_input
         .lines()
         .map(|line| {
-            let x = line.find(|c: char| c.is_ascii_digit()).unwrap();
-            let x2 = line.rfind(|c: char| c.is_ascii_digit()).unwrap();
+            let chars: Vec<char> = line.chars().collect();
 
-            let a: Vec<char> = line.chars().collect();
-            let b = a[x].to_string();
-            let b2 = a[x2].to_string();
+            let first_digit = chars
+                .iter()
+                .find(|c| c.is_ascii_digit())
+                .and_then(|n| n.to_digit(10))
+                .unwrap();
 
-            (b.parse::<u32>().unwrap() * 10) + b2.parse::<u32>().unwrap()
+            let second_digit = chars
+                .iter()
+                .rfind(|c| c.is_ascii_digit())
+                .and_then(|n| n.to_digit(10))
+                .unwrap();
+
+            (first_digit * 10) + second_digit
         })
         .sum()
 }
 
 fn part_two(puzzle_input: &str) -> u32 {
-    let nums = vec![
+    let word_to_number_map: HashMap<&str, u32> = [
         ("one", 1),
         ("two", 2),
         ("three", 3),
@@ -32,54 +41,73 @@ fn part_two(puzzle_input: &str) -> u32 {
         ("seven", 7),
         ("eight", 8),
         ("nine", 9),
-    ];
+    ]
+    .into_iter()
+    .collect();
 
     puzzle_input
         .lines()
         .map(|line| {
-            let line_chars: Vec<char> = line.chars().collect();
+            let chars: Vec<char> = line.chars().collect();
 
-            let word_stuff_left = nums
+            let (left_word_index, left_word_value) = word_to_number_map
                 .iter()
-                .map(|(word, value)| (word, value, line.find(word)))
-                .min_by_key(|(_, _, idx)| idx.unwrap_or(usize::MAX))
+                .map(|(word, value)| (line.find(word), value))
+                .min_by_key(|(index, _)| index.unwrap_or(usize::MAX))
                 .unwrap();
-            let word_stuff_right = nums
+            let (right_word_index, right_word_value) = word_to_number_map
                 .iter()
-                .map(|(word, value)| (word, value, line.rfind(word)))
-                .max_by_key(|(_, _, idx)| idx.unwrap_or(usize::MIN))
+                .map(|(word, value)| (line.rfind(word), value))
+                .max_by_key(|(index, _)| index.unwrap_or(usize::MIN))
                 .unwrap();
 
-            let left_most_digit = line.find(|c: char| c.is_ascii_digit());
-            let right_most_digit = line.rfind(|c: char| c.is_ascii_digit());
+            let left_digit_index = line.find(|c: char| c.is_ascii_digit());
+            let right_digit_index = line.rfind(|c: char| c.is_ascii_digit());
 
-            let first_digit = if word_stuff_left.2.unwrap_or(usize::MAX)
-                < left_most_digit.unwrap_or(usize::MAX)
-            {
-                *word_stuff_left.1
-            } else {
-                line_chars
-                    .get(left_most_digit.unwrap())
-                    .unwrap()
-                    .to_string()
-                    .parse()
-                    .unwrap()
-            };
+            let first_digit =
+                if left_word_index.unwrap_or(usize::MAX) < left_digit_index.unwrap_or(usize::MAX) {
+                    *left_word_value
+                } else {
+                    chars[left_digit_index.unwrap()].to_digit(10).unwrap()
+                };
 
-            let second_digit = if word_stuff_right.2.unwrap_or(usize::MIN)
-                > right_most_digit.unwrap_or(usize::MIN)
+            let second_digit = if right_word_index.unwrap_or(usize::MIN)
+                > right_digit_index.unwrap_or(usize::MIN)
             {
-                *word_stuff_right.1
+                *right_word_value
             } else {
-                line_chars
-                    .get(right_most_digit.unwrap())
-                    .unwrap()
-                    .to_string()
-                    .parse()
-                    .unwrap()
+                chars[right_digit_index.unwrap()].to_digit(10).unwrap()
             };
 
             (first_digit * 10) + second_digit
         })
         .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let sample_input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+
+        assert_eq!(part_one(sample_input), 142);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let sample_input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        assert_eq!(part_two(sample_input), 281);
+    }
 }
