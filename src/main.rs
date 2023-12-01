e5eb9438357cc0bd6e2ed7a2ab214349204bd48a
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2023-11-30 22:49:14 -0700

    Solve day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = include_str!("../puzzle_input.txt");
+
+    // Part 1
+    let x: u32 = puzzle_input
+        .lines()
+        .map(|line| {
+            let x = line.find(|c: char| c.is_digit(10)).unwrap();
+            let x2 = line.rfind(|c: char| c.is_digit(10)).unwrap();
+
+            let a: Vec<char> = line.chars().collect();
+            let b = a[x].to_string();
+            let b2 = a[x2].to_string();
+
+            // let a = line.get(x).unwrap();
+            // let b = line.get(x2).unwrap();
+            println!("{} {}", &b, &b2);
+            (b.parse::<u32>().unwrap() * 10) + b2.parse::<u32>().unwrap()
+        })
+        .sum();
+    println!("{x}");
+
+    // Part 2
+    let nums = vec![
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+
+    let ans: u32 = puzzle_input
+        .lines()
+        .map(|line| {
+            let line_chars: Vec<char> = line.chars().collect();
+
+            let word_stuff_left = nums
+                .iter()
+                .map(|(word, value)| (word, value, line.find(word)))
+                .min_by_key(|(_, _, idx)| idx.unwrap_or(usize::MAX))
+                .unwrap();
+            let word_stuff_right = nums
+                .iter()
+                .map(|(word, value)| (word, value, line.rfind(word)))
+                .max_by_key(|(_, _, idx)| idx.unwrap_or(usize::MIN))
+                .unwrap();
+
+            let left_most_digit = line.find(|c: char| c.is_digit(10));
+            let right_most_digit = line.rfind(|c: char| c.is_digit(10));
+
+            // println!("{}", line);
+            // dbg!(word_stuff_left.1, word_stuff_right.1);
+            // dbg!(left_most_digit, right_most_digit);
+
+            let first_digit = if (word_stuff_left.2.unwrap_or(usize::MAX)
+                < left_most_digit.unwrap_or(usize::MAX))
+            {
+                *word_stuff_left.1
+            } else {
+                line_chars
+                    .get(left_most_digit.unwrap())
+                    .unwrap()
+                    .to_string()
+                    .parse()
+                    .unwrap()
+            };
+
+            let second_digit = if (word_stuff_right.2.unwrap_or(usize::MIN)
+                > right_most_digit.unwrap_or(usize::MIN))
+            {
+                *word_stuff_right.1
+            } else {
+                line_chars
+                    .get(right_most_digit.unwrap())
+                    .unwrap()
+                    .to_string()
+                    .parse()
+                    .unwrap()
+            };
+
+            // println!("{} {}", first_digit, second_digit);
+            (first_digit * 10) + second_digit
+        })
+        .sum();
+    println!("{}", ans);
 }
